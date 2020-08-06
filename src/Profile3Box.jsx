1859f6a7584d892195bd62b1ee6c7da2dfa80613
Alexander Padilla <yamop.16@gmail.com>
2020-08-05 20:42:20 -0500

    Added 3box basic services

--- src/Profile3Box.jsx
+++ src/Profile3Box.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import Storage from "./services/StorageService"
 
 export default function Profile3Box() {
     const settings = useSelector(state => state.retrospective.settings3box)
+    const [spaces, setSpace] = useState([]);
 
-    return <div>Profile {settings.account}</div>
+    useEffect(
+        () => {
+            async function fetchRetrospectives() {
+                const storage = new Storage();
+                const sAvailable = await storage.getRetrospectives(settings.account);
+                console.log("spaces");
+                console.log(sAvailable);
+                setSpace(sAvailable);
+            }
+          
+            fetchRetrospectives();
+        }, []);
+
+    console.log(settings.thread);
+    
+    return (
+        <div className="panel-landing" id="section-1">
+            <h1 className="landing-heading">Welcome {settings.profile.name}!</h1>
+            <p className="lead">
+                {settings.account}
+            </p>
+            <p className="lead">
+                Retrospectives available...
+            </p>
+            <ul>
+                {spaces.map(item => (
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
