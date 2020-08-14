dcadfc0765c0c1e56c4f25d33023ac8cbce8036d
Hector Benitez <hector.benitez.fletes@gmail.com>
2020-08-14 09:22:21 -0500

    input new DRetro

--- src/Profile3Box.jsx
+++ src/Profile3Box.jsx
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom'
 export default function Profile3Box () {
   const settings = useSelector(state => state.retrospective.settings3box)
   const [spaces, setSpace] = useState([])
+  const [newDRetroName, setNewDRetroName] = useState('')
 
   useEffect(() => {
     async function fetchRetrospectives () {
@@ -21,40 +22,49 @@ export default function Profile3Box () {
     fetchRetrospectives()
   }, [])
 
-  console.log(settings.thread)
+  function handleCreateDRetro () {
+    console.log('Create DRetro', newDRetroName)
+    setNewDRetroName('')
+  }
 
   return (
     <div>
-      
-            <div class='d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom'>
-              <h1 class='h2'>Dashboard</h1>
-              <div class='btn-toolbar mb-2 mb-md-0'>
-                <div class='btn-group mr-2'>
-                  <button class='btn btn-sm btn-outline-secondary'>
-                    Share
-                  </button>
-                  <button class='btn btn-sm btn-outline-secondary'>
-                    Export
-                  </button>
-                </div>
-                <button class='btn btn-sm btn-outline-secondary dropdown-toggle'>
-                  <span data-feather='calendar'></span>
-                  This week
-                </button>
-              </div>
+      <div class='d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom'>
+        <h1 class='h2'>Dashboard</h1>
+        <div class='btn-toolbar mb-2 mb-md-0'>
+          <div class='input-group mb-3'>
+            <input
+              type='text'
+              class='form-control'
+              placeholder='New DRetro Name'
+              aria-label='New DRetro Name'
+              aria-describedby='button-addon2'
+              value={newDRetroName}
+              onChange={e => setNewDRetroName(e.target.value)}
+            />
+            <div class='input-group-append'>
+              <button
+                class='btn btn-primary'
+                type='button'
+                onClick={handleCreateDRetro}
+              >
+                Create
+              </button>
             </div>
+          </div>
+        </div>
+      </div>
 
-            <h1 className='landing-heading'>
-              Welcome {settings.profile.name}!
-            </h1>
-            <p className='lead'>{settings.account}</p>
-            <p className='lead'>Retrospectives available...</p>
-            <ul>
-              {spaces.map(item => (
-                <li key={item}><Link to={`/game/${item}`}>{item}</Link></li>
-              ))}
-            </ul>
-          
+      <h1 className='landing-heading'>Welcome {settings.profile.name}!</h1>
+      <p className='lead'>{settings.account}</p>
+      <p className='lead'>Retrospectives available...</p>
+      <ul>
+        {spaces.map(item => (
+          <li key={item}>
+            <Link to={`/game/${item}`}>{item}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
