8019d61f71be87527e7ec9a7ab3963dba452226c
Alexander Padilla <yamop.16@gmail.com>
2020-08-14 10:34:09 -0500

    Added input for create new retro

--- src/Profile3Box.jsx
+++ src/Profile3Box.jsx
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom'
 export default function Profile3Box () {
   const settings = useSelector(state => state.retrospective.settings3box)
   const [spaces, setSpace] = useState([])
+  const [newDRetroName, setNewDRetroName] = useState('')
 
   useEffect(() => {
     async function fetchRetrospectives () {
@@ -22,49 +23,49 @@ export default function Profile3Box () {
   }, [])
 
   const onClickHandler = async () => {
-    const spaceName = "hackfs"
     const storage = new Storage()
-    const nSpace = await storage.createRetrospective(settings.box, spaceName)
+    const nSpace = await storage.createRetrospective(settings.box, newDRetroName)
     let newSpaces = [...spaces, nSpace['_name']]
     setSpace(newSpaces)
   }
 
-  console.log(settings.thread)
-
   return (
     <div>
-      
-            <div class='d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom'>
-              <h1 class='h2'>Dashboard</h1>
-              <div class='btn-toolbar mb-2 mb-md-0'>
-                <div class='btn-group mr-2'>
-                  <button class='btn btn-sm btn-outline-secondary'>
-                    Share
-                  </button>
-                  <button class='btn btn-sm btn-outline-secondary'>
-                    Export
-                  </button>
-                </div>
-                <button class='btn btn-sm btn-outline-secondary dropdown-toggle'>
-                  <span data-feather='calendar'></span>
-                  This week
-                </button>
-              </div>
+      <div class='d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom'>
+        <h1 class='h2'>Dashboard</h1>
+        <div class='btn-toolbar mb-2 mb-md-0'>
+          <div class='input-group mb-3'>
+            <input
+              type='text'
+              class='form-control'
+              placeholder='New DRetro Name'
+              aria-label='New DRetro Name'
+              aria-describedby='button-addon2'
+              value={newDRetroName}
+              onChange={e => setNewDRetroName(e.target.value)}/>
+              
+            <div class='input-group-append'>
+              <button
+                class='btn btn-primary'
+                type='button'
+                onClick={onClickHandler}>
+                  Create
+              </button>
             </div>
+          </div>
+        </div>            
+      </div>
 
-            <h1 className='landing-heading'>
-              Welcome {settings.profile.name}!
-            </h1>
-            <p className='lead'>{settings.account}</p>
-            <p className='lead'>Retrospectives available...</p>
-            <ul>
-              {spaces.map(item => (
-                <li key={item}><Link to={`/game/${item}`}>{`${item}`.replace('dretros-','')}</Link></li>
-              ))}
-            </ul>
-            <button onClick={onClickHandler}>
-              add space
-            </button>
+      <h1 className='landing-heading'>
+        Welcome {settings.profile.name}!
+      </h1>
+      <p className='lead'>{settings.account}</p>
+      <p className='lead'>Retrospectives available...</p>
+      <ul>
+        {spaces.map(item => (
+          <li key={item}><Link to={`/game/${item}`}>{`${item}`.replace('dretros-','')}</Link></li>
+        ))}
+      </ul>
     </div>
   )
 }
